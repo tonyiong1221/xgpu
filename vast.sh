1c1b7340a250108c2253932993c5fe195858112d
tonyiong1221 <58626915+tonyiong1221@users.noreply.github.com>
2023-10-14 22:05:56 +0800

    vast.sh

--- vast.sh
+++ vast.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xA67A432ac8E0D3e56b7aD5b63027774c8750e194/g' config.conf
 sleep 5
 echo "---------------------------"
 
